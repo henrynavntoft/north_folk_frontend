acccf789645172d177b20f7d0d7768ebb5ed31a5
henrynavntoft <henrylnavntoft@gmail.com>
2022-12-15 09:44:25 +0100

    yes sir

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-900px";
+    document.getElementById("navbar").style.top = "-100vh";
   }
 }
 
